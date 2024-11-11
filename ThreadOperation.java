fea72a825c8ac0ac257ec7281b6b467137262824
IsaiahTJames <IsaiahTJames@gmail.com>
2024-11-10 23:27:01 -0700

    Add files via upload
    Part 2

--- ThreadOperation.java
+++ ThreadOperation.java
@@ -1,63 +1,73 @@
+
+
 public class ThreadOperation extends Thread
 {
-	//matrices A, B is inputed. Then C is the result
+	//Matrices A and B are the initial input matrices, C is the result of both.
 	private int[][] A;
 	private int[][] B;
 	private int[][] C;
-	private String quadrant;			//quadrant is specified where the threads will be processed in.
+	private String quadrant;		//represents the quadrant that the thread will process.
 
 	//constructors initializes matrices and the quadrant for the thread
 	public ThreadOperation(int[][] A, int[][] B, int[][] C, String quadrant)
 	{
-		this.A = A;
+		this.A = A;		
 		this.B = B;
 		this.C = C;
 		this.quadrant = quadrant;
+
 	}
-	//run method defines the work done by the thread when it starts
+	public int[] getQuadrantIndexes()
+	{
+		int rows = A.length;				//total number of rows in matrix
+		int columns = A[0].length;			//total number of columns in the matrix
+		int midRow = rows / 2;				//midpoint of rows, divide the top and bottom
+		int midCol = columns / 2;			//midpoint of the columns, divide the left and right
+
+		//initialize vairables for the row and columns
+		int rowStart = 0, rowEnd = 0, colStart = 0, colEnd = 0;
+		//determins the row and column indexes based on the quarants
+		if(quadrant.equals("upperLeft"))
+		{
+			rowStart = 0;
+			rowEnd = midRow;
+			colStart = 0;
+			colEnd = midCol;
+		} else if(quadrant.equals("upperRight"))
+		{
+			rowStart = 0;
+			rowEnd = midRow;
+			colStart = midCol;
+			colEnd = columns;
+		} else if(quadrant.equals("bottomLeft"))
+		{
+			rowStart = midRow;
+			rowEnd = rows;
+			colStart = 0;
+			colEnd = midCol;
+		} else{
+			rowStart = midRow;
+			rowEnd = rows;
+			colStart = midCol;
+			colEnd = columns;
+		}
+		return new int[]{rowStart, rowEnd, colStart, colEnd};		//return the calculated row and column boundares as an array
+	}
 	@Override
+	//runs the threads task of addition, with the matrices A and B = C matrix for the quadrants
 	public void run()
 	{
-		int rowStart = 0, rowEnd = 0, colStart = 0, colEnd = 0;
-
-		//calculates the midpoints of the rows and colums to split the matrix into quadrants.
-		int midRow = A.length / 2;		//middle row, dividing the top and bottom halves
-		int midCol = A[0].length / 2;		//middle column, dividing the left and right halves
-
-		//the boundaries (start and the end) of the indices bassed on the quadrant
-		switch(quadrant)
+		int[] indexes = getQuadrantIndexes();
+		int rowStart = indexes[0], rowEnd = indexes[1];		//the boundaries for the threads in the designated quadrants.
+		int colStart = indexes[2], colEnd = indexes[3];
 		{
-		case "00":			//top-left
-			rowStart = 0;					//first row
-			rowEnd = midRow;				//ends the row at the midpoint
-			colStart = 0;					//first column
-			colEnd = midCol;				//ends the column at the midpoint
-			break;
-		case "01":			//top-right
-			rowStart = 0;					//first row
-			rowEnd = midRow;				//end at the midpoint row
-			colStart = midCol;				//starts from the midpoint column
-			colEnd = A[0].length;			//ends at the last column
-			break;
-		case "10":				//bottom-left
-			rowStart = midRow;				//starts from the midpoint
-			rowEnd = A.length;				//end at the last row
-			colStart = 0;					//starts from the first column
-			colEnd = midCol;				//ends at the midpoint of the column
-			break;
-		case "11":				//bottom-right
-			rowStart = midRow;				//starts at the midpoint row
-			rowEnd = A.length;				//end at the last row
-			colStart = midCol;				//starts from the midpoint column
-			colEnd = A[0].length;			//ends at the last column
-			break;
-		}
-		for(int i = rowStart; i < rowEnd; i++)			//loops through each row in the particular quadrant
-		{
-			for(int j = colStart; j < colEnd; j++)		//loops through each column in the particul quadrant
+			for(int i = rowStart; i < rowEnd; i++)			//matrix addition will take place in the specfic row and column within the boundries
 			{
-				C[i][j] = A[i][j] + B[i][j];			//result C = A and B (row and col)
-			}	
+				for(int j = colStart; j < colEnd; j++)
+				{
+					C[i][j] = A[i][j] + B[i][j];		//the elements A and B are stored in C
+				}
+			}
 		}
 	}
 }
