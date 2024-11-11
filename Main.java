324855b9e58620fbae8c0f6c85ec3333def3f772
IsaiahTJames <IsaiahTJames@gmail.com>
2024-11-10 23:31:59 -0700

    part 2

--- Main.java
+++ Main.java
@@ -1,35 +1,10 @@
 /*
 Name: Isaiah James
-Date: November 2, 2024
+Date: November 9, 2024
 Class: Interm Programming
-Purpose: implement multi-threaded matrix addition, demonstrates how to divide a larger
-problem into a smaller quadrants. Each seperate thread is processed simultaneously, results
-in more efficient and optimized. Reducing the execution time, memory management, and dividing tasks. 
-
-This code is provided to give you a
-starting place. It should be modified.
-No further imports are needed.
-To earn full credit, you must also
-answer the following question:
-
-Q1: One of the goals of multi-threading
-is to minimize the resource usage, such
-as memory and processor cycles. In three
-sentences, explain how multi-threaded
-code accomplishes this goal. Consider
-writing about blocking on I/O, multicore 
-machines, how sluggish humans are,
-threads compared to processes, etcetera,
-and connect these issues to 
-multi-threading.
-
-Answer: 
-mult-threading minimiezes resources by having the tasks run concurrently on
-a multi-core processor, this maximizes CPU efficiency. Threads share memory space, which is 
-different from processes. Threads elimates the act of copying memory and is faster, this saves
-memory and the processor cylces. By using threads, we can continue to execute multiple tasks while 
-waiting on I/O expections, reduces the delays and handles tasks more efficiently.
-
+Purpose: part 2 of the Matrix Addition is to implement a multithreading solution, that adds two matrices by divinding
+the work into four quadrants, each is a seperate thread. Each thread A and B is assigned to a quadrant and stored in C.
+This demonstrates the use of threads for divinng tasks within a matrix operation setting. 
 */
 import java.io.IOException;
 import java.io.File;
@@ -50,19 +25,38 @@ public class Main
 		{		
 			Scanner s = new Scanner(new File(filename));		//opens the file name using the scanner
 
-			int row = s.nextInt();			//reads the number of rows from the file
-			int col = s.nextInt();			//reads the number of columns from the file
+			int rows = s.nextInt();			//reads the number of rows from the file
+			int cols = s.nextInt();			//reads the number of columns from the file
 
-			//reads the matrices of A and B from the file, then creates the Matrix C for the results.
-			int[][] A = matrixFromFile(row, col, s);		//Matrix A
-			int[][] B = matrixFromFile(row, col, s);		//Matrix B
-			int[][] C = new int[row][col];					//Matrix C (results)
+			//initialize matrices A, B an C
+			int[][] A = new int[rows][cols];
+			int[][] B = new int[rows][cols];
+			int[][] C = new int[rows][cols];
+
+			//Reads matrix A
+			for(int i = 0; i < rows; i++)
+			{
+				for(int j = 0; j < cols; j++)
+				{
+					A[i][j] = s.nextInt();
+				}
+			}
+			//Reads matrix B
+			for(int i = 0; i < rows; i++)
+			{
+				for(int j = 0; j < cols; j++)
+				{
+					B[i][j] = s.nextInt();
+				}
+			}
+
+			s.close();	//scanner close
 
 			//four threads (t1-t4) in a quadrant plane
-			ThreadOperation t1 = new ThreadOperation(A, B, C, "00");		//top-left quadrant
-			ThreadOperation t2 = new ThreadOperation(A, B, C, "01");		//top-right quadrant
-			ThreadOperation t3 = new ThreadOperation(A, B, C, "10");		//bottom-left quadrant
-			ThreadOperation t4 = new ThreadOperation(A, B, C, "11");		//bottom-right quadrant
+			ThreadOperation t1 = new ThreadOperation(A, B, C, "upperLeft");
+			ThreadOperation t2 = new ThreadOperation(A, B, C, "upperRight");		
+			ThreadOperation t3 = new ThreadOperation(A, B, C, "bottomLeft");		
+			ThreadOperation t4 = new ThreadOperation(A, B, C, "bottomRight");		
 
 			//starts each of the threads
 			t1.start();
@@ -77,11 +71,11 @@ public class Main
 
 			print2dArray(C);		//prints the final result in matrix C
 
-			s.close();			//scanner close
-
 		} catch (IOException e) {				//file related exceptions
+			System.out.println("Error reading the fil. Please check the file path and format.");
 			e.printStackTrace();
 		} catch (InterruptedException e){		//exceptions from thread interruptions
+			System.out.println("A thread was interrupted.");
 			e.printStackTrace();
 		}
 	}
@@ -96,18 +90,5 @@ public class Main
 			}
 			System.out.println();				//new line after each row for the matrix
 		}
-	}
-	//method to read a matrix from the file, using the columns and rows.
-	public static int[][] matrixFromFile(int row, int col, Scanner s)
-	{
-		int[][] matrix = new int[row][col];		//initializes the matrix as a 2D array
-		for(int i = 0; i < row; i++)			//loops through each row as i
-		{
-			for(int j = 0; j < col; j++)		//loops though each column, within each row
-			{
-				matrix[i][j] = s.nextInt();		//reads and stores the next integer from the file
-			}
-		}
-		return matrix;			//returns the given matrix
 	}
 }
